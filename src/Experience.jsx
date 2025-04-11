16bcf1249ffb08abd8ca7d45e2aec33903783de0
AntonMhnk <anton.mhnk@yandex.ru>
2025-04-11 15:31:31 +0500

    fullscreen test

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,10 +3,30 @@ import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import { Player } from "./Player.jsx";
 import useGame from "./stores/useGame.jsx";
+import { viewport, isTMA } from "@telegram-apps/sdk";
+import { useEffect } from "react";
 
 export default function Experience() {
 	const blocksCount = useGame((state) => state.blocksCount);
 	const blocksSeed = useGame((state) => state.blocksSeed);
+
+	useEffect(() => {
+		async function initTg() {
+			if (await isTMA()) {
+				init();
+
+				if (viewport.mount.isAvailable()) {
+					await viewport.mount();
+					viewport.expand();
+				}
+
+				if (viewport.requestFullscreen.isAvailable()) {
+					await viewport.requestFullscreen();
+				}
+			}
+		}
+		initTg();
+	}, []);
 
 	return (
 		<>
